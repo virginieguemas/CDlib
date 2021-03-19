3d15f094ccd95f91125b080b6c104c359892d0ef
Virginie Guemas <virginie.guemas@meteo.fr>
2021-03-19 16:04:06 +0100

    Ensure that psi = 0 if zeta = 0 in PSI and np.where (zeta<0 ... is not necessary since psi is np.nan for zeta_unstab = np.nan

--- CDlib.py
+++ CDlib.py
@@ -799,8 +799,9 @@ def PSI(zeta, gamma=5, stab=None, unstab=None) :
      phiM = (1 - 16*zeta_unstab)**(-0.25)
      phiH = (1 - 16*zeta_unstab)**(-0.5)
 
-     psiM = np.where(zeta<0, 2*unp.log((1+phiM**(-1))/2) + unp.log((1+phiM**(-2))/2) - 2*unp.arctan(phiM**(-1)) + np.pi/2, np.nan)
-     psiH = np.where(zeta<0, 2*unp.log((1+phiH**(-1))/2), np.nan)
+     psiM = 2*unp.log((1+phiM**(-1))/2) + unp.log((1+phiM**(-2))/2) - 2*unp.arctan(phiM**(-1)) + np.pi/2
+     psiH = 2*unp.log((1+phiH**(-1))/2)
+     # psi is np.nan when zeta >= 0
    ################################
    elif unstab == 'holtslag1990':
      # I need to integrate the phi
@@ -815,10 +816,11 @@ def PSI(zeta, gamma=5, stab=None, unstab=None) :
    elif unstab == 'fairall1996':
      y = (1 - 12.87*zeta_unstab)**(1/3)
 
-     psi = np.where(zeta<0, 1.5*unp.log((y**2+y+1)/3) - unp.sqrt(3)*unp.arctan((2*y+1)/unp.sqrt(3)) + np.pi/unp.sqrt(3), np.nan)
+     psi = 1.5*unp.log((y**2+y+1)/3) - unp.sqrt(3)*unp.arctan((2*y+1)/unp.sqrt(3)) + np.pi/unp.sqrt(3)
 
-     psiM = np.where(zeta<0, 1/(1+zeta_unstab**2)*PSI(zeta_unstab, stab = stab, unstab = 'businger-dyer')[0] + zeta_unstab**2/(1+zeta_unstab**2)*psi, np.nan)
-     psiH = np.where(zeta<0, 1/(1+zeta_unstab**2)*PSI(zeta_unstab, stab = stab, unstab = 'businger-dyer')[1] + zeta_unstab**2/(1+zeta_unstab**2)*psi, np.nan)
+     psiM = 1/(1+zeta_unstab**2)*PSI(zeta_unstab, stab = stab, unstab = 'businger-dyer')[0] + zeta_unstab**2/(1+zeta_unstab**2)*psi
+     psiH = 1/(1+zeta_unstab**2)*PSI(zeta_unstab, stab = stab, unstab = 'businger-dyer')[1] + zeta_unstab**2/(1+zeta_unstab**2)*psi
+     # psi is np.nan when zeta >= 0
    ################################
    elif unstab == 'grachev2000':
      a={'m':10.15,'h':34.15}
@@ -826,16 +828,17 @@ def PSI(zeta, gamma=5, stab=None, unstab=None) :
      for s in ('m','h'):
        y = (1 - a[s]*zeta_unstab)**(1/3)
 
-       psi[s] = np.where(zeta<0, 1.5*unp.log((y**2+y+1)/3) - unp.sqrt(3)*unp.arctan((2*y+1)/unp.sqrt(3)) + np.pi/unp.sqrt(3), np.nan)
+       psi[s] = 1.5*unp.log((y**2+y+1)/3) - unp.sqrt(3)*unp.arctan((2*y+1)/unp.sqrt(3)) + np.pi/unp.sqrt(3)
 
-     psiM = np.where(zeta<0, 1/(1+zeta_unstab**2)*PSI(zeta_unstab, stab = stab, unstab = 'businger-dyer')[0] + zeta_unstab**2/(1+zeta_unstab**2)*psi['m'], np.nan)
-     psiH = np.where(zeta<0, 1/(1+zeta_unstab**2)*PSI(zeta_unstab, stab = stab, unstab = 'businger-dyer')[1] + zeta_unstab**2/(1+zeta_unstab**2)*psi['h'], np.nan)
+     psiM = 1/(1+zeta_unstab**2)*PSI(zeta_unstab, stab = stab, unstab = 'businger-dyer')[0] + zeta_unstab**2/(1+zeta_unstab**2)*psi['m']
+     psiH = 1/(1+zeta_unstab**2)*PSI(zeta_unstab, stab = stab, unstab = 'businger-dyer')[1] + zeta_unstab**2/(1+zeta_unstab**2)*psi['h']
+     # psi is np.nan when zeta >= 0
    ################################  
    elif  unstab == 'beljaars-holtslag':
      a,b,c,d = 1.,0.667,5.,0.35
 
-     psiM = np.where(zeta<0, -(a * zeta_unstab + b*(zeta_unstab - c/d) * unp.exp(-d * zeta_unstab) + b*c/d), np.nan)
-     psiH = np.where(zeta<0, -(a * zeta_unstab + b*(zeta_unstab - c/d) * unp.exp(-d * zeta_unstab) + b*c/d), np.nan)
+     psiM = -(a * zeta_unstab + b*(zeta_unstab - c/d) * unp.exp(-d * zeta_unstab) + b*c/d)
+     psiH = -(a * zeta_unstab + b*(zeta_unstab - c/d) * unp.exp(-d * zeta_unstab) + b*c/d)
      # Beljaars and Holtslag do not propose an option for unstable cases but only for stable cases. This formulation is here
      # only to reproduce Elvidge et al (2016).
    else:
@@ -879,6 +882,9 @@ def PSI(zeta, gamma=5, stab=None, unstab=None) :
      sys.exit('This option for stable cases is not coded yet.')
    ################################
    ################################
+   # To avoid np.nan for zeta = 0
+   psiM = np.where (zeta == 0., 0., psiM)
+   psiH = np.where (zeta == 0., 0., psiH)
    
    return (psiM,psiH)
 ################################################################################
